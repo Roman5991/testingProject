dbc00c2d8d845945b3cc4d002368b32114da8242
Roman <fediuninrom5991@gmail.com>
2022-07-10 15:07:55 +0200

    js connected

---
+++ main.js
@@ -0,0 +1,21 @@
+//the first way
+
+// let dropdown = document.querySelector('.dropdown_menu');
+// console.log(dropdown);
+// let btn = document.getElementById('btn1');
+
+// btn.addEventListener('click', function () {
+//     let disp = dropdown.style.display;
+//     if (disp === 'block') {
+//         dropdown.setAttribute('style', 'display: none;');
+//         console.log('Hi');
+//     }
+//     else dropdown.setAttribute('style', 'display: block;transition: ease 0.5s;')
+
+// });
+
+
+
+//the second way 
+
+
